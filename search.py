6eec84284887d4e967a5404360e064ccaba05c09
Ali <aliradha@umich.edu>
2013-06-05 15:44:35 -0400

    Added random point search for color segmentation and inversion bit

--- search.py
+++ search.py
@@ -1,4 +1,8 @@
-from random import randint # for rgb ranges
+# Implementations of search subclasses
+
+from random import random # for [0, 1) random values
+from random import choice # for random flip bit value
+from random import randint # for color ranges
 from Grammer import Search # Abstract Base Class
 import pylab # for plotting
 
@@ -24,26 +28,33 @@ class randomSearch(Search):
 
         print "Running random search"
         
-        curBestRGB = self.randRgbRange()
-        parameters.setRgbRanges(curBestRGB)
+        curBestColorRange = self.randPointsColorRange(imageData)
+	curBestFlipBit = choice([True, False])
+        parameters.setColorRanges(curBestColorRange)
+	parameters.setFlipBit(curBestFlipBit)
         mask = self.segmenter.segmentImage(imageData, parameters)
         curBestFit = self.fitness.findFitness(mask, idealMaskData, parameters)
         
         self.iterations = int(raw_input("How many iterations do you want random search to run: "))
         
-                # if plotting enabled, make a list
+        # if plotting enabled, make a list
         if plot == True:
             self.fitnessList = [curBestFit]
             self.iterationsList = [0]
         
         for i in range(self.iterations):
-            newRGB =  self.randRgbRange()
-            parameters.setRgbRanges(newRGB)
+            newColorRange =  self.randPointsColorRange(imageData)
+	    newFlipBit = choice([True, False])
+            
+	    parameters.setColorRanges(newColorRange)
+	    parameters.setFlipBit(newFlipBit)
+	    
             mask = self.segmenter.segmentImage(imageData, parameters)
             newFit = self.fitness.findFitness(mask, idealMaskData, parameters)
             
             if newFit < curBestFit:
-                curBestRGB = newRGB
+                curBestColorRange = newColorRange
+		curBestFlipBit = newFlipBit
                 curBestFit = newFit
                 
                 #append to list if plotting enabled
@@ -54,22 +65,34 @@ class randomSearch(Search):
             print "Iteration " + str(i + 1) + ", Fitness: " + str(curBestFit)
 
         
-        parameters.setRgbRanges(curBestRGB)
+        parameters.setColorRanges(curBestColorRange)
+        parameters.setFlipBit(curBestFlipBit)
         
         if plot == True:
             self.plotSearch()
         
         return parameters
     
-    def randRgbRange(self):
-        # Effects: returns three 2-tuples of valid RGB Min-Max's
-        rgbRangeList = []
+    def randColorRange(self):
+        # Effects: returns three 2-tuples of valid [0, 1] color range Min-Max's
+        colorRangeList = []
         for i in range(6):
-            rgbRangeList.append(randint(0,255))
+            colorRangeList.append(random())
         for i in [1,3,5]:
-            if rgbRangeList[i] < rgbRangeList[i - 1]:
-                rgbRangeList[i] = randint(rgbRangeList[i - 1], 255)
-        return (rgbRangeList[0:2], rgbRangeList[2:4], rgbRangeList[4:6])
+            while colorRangeList[i] < colorRangeList[i - 1]:
+                colorRangeList[i] = random()
+
+        return (colorRangeList[0:2] , colorRangeList[2:4], colorRangeList[4:6])
+        
+    def randPointsColorRange(self, imageData):
+        # Requires: imageData holds 3-tupled image data in range [0, 1]
+        # Effects: returns three 2-tuples of valid [0, 1] color ranges
+        #          using randomly sampled points from the image
+        p1 = imageData[randint(0, len(imageData))]
+        p2 = imageData[randint(0, len(imageData))]
+        
+        tuples = lambda x : (min(x), max(x))
+        return (sorted([p1[0], p2[0]]), sorted([p1[1], p2[1]]), sorted([p1[2], p2[2]]))
     
     def plotSearch(self):
         print self.fitnessList
