716f006f3513b371675bbd0475967abb670feb0c
Ali <aliradha@umich.edu>
2013-06-11 15:19:17 -0400

    Edited RME clause for fitness

--- fitness.py
+++ fitness.py
@@ -10,11 +10,19 @@ class absDiffFitness(Fitness):
         pass
     
     def findFitness(self, generatedMaskData, idealMaskData, parameters):
-        # Requires: both inputs are RGB (0 - 255) based images
-        # Effects:  returns # of black/white pixels in generated mask
-        #           that differ from ideal mask
+        # Requires: -both inputs are RGB (0 - 255) based images
+        # Effects:  -returns # of black/white pixels in generated mask
+        #            that differ from ideal mask
+        #           -if upperLimit in parameters is set to a non-default value, then
+        #            the fitness function will stop calculations
+        #            if the fitness exceeds the upper limit and return
+        #            upper limit + 1 as the fitness; this is implemented
+        #            so as to make calculations faster when used in conjunction
+        #            with another algorithm such as random search
         totalDiff = 0
         for i in range(len(generatedMaskData)):
             totalDiff += abs(generatedMaskData[i][0]/255 - idealMaskData[i][0]/255)
+            if totalDiff > parameters.upperLimit:
+                return totalDiff
         return totalDiff
 
